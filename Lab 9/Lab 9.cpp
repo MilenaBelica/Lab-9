b631570ce86bcddac440dcca50e4316df3c6834d
MilenaBelica <milena.belica@outlook.com>
2025-12-16 16:33:29 +0100

    zmiany

--- Lab 9/Lab 9.cpp
+++ Lab 9/Lab 9.cpp
@@ -1,1 +1,35 @@
-﻿
+﻿#include <iostream>
+using namespace std;
+
+int poleprostokata(int x, int y);
+
+struct Zadanie1 {
+	int x;
+	int y;
+};
+struct Zadanie2 {
+	int a;
+	int b;
+};
+int main() {
+
+	//zadanie 1
+	Zadanie1 zmienna;
+	zmienna.x = 10;
+	zmienna.y = 20;
+
+	cout << zmienna.x << "\t" << zmienna.y << endl;
+
+	//zadanie 2
+	Zadanie2 prostokat;
+	prostokat.a = 10;
+	prostokat.b = 20;
+
+	cout << poleprostokata(prostokat.a, prostokat.b) << endl;
+
+	//zadanie 3
+
+}
+int poleprostokata(int x, int y) {
+	return x * y;
+}
