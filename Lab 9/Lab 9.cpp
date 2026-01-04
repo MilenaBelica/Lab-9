06e84a94e12624abe910c43c9a0970ef65dcb594
MilenaBelica <milena.belica@outlook.com>
2026-01-04 17:06:56 +0100

    zmiany

--- Lab 9/Lab 9.cpp
+++ Lab 9/Lab 9.cpp
@@ -1,8 +1,6 @@
 ﻿#include <iostream>
 using namespace std;
 
-int poleprostokata(int x, int y);
-
 struct Zadanie1 {
 	int x;
 	int y;
@@ -11,8 +9,23 @@ struct Zadanie2 {
 	int a;
 	int b;
 };
-int main() {
+struct Trojkat {
+	int a;
+	int b;
+	int c;
+};
+struct Konto {
+	int numer;
+	int saldo;
+};
 
+int poleprostokata(int x, int y);
+void funkcja(Trojkat trojkat_1, Trojkat* trojkat_2);
+void zaktualizuj(Konto noweDane, Konto* stareKonto);
+
+
+int main() {
+	/*
 	//zadanie 1
 	Zadanie1 zmienna;
 	zmienna.x = 10;
@@ -21,6 +34,7 @@ int main() {
 	cout << zmienna.x << "\t" << zmienna.y << endl;
 
 	//zadanie 2
+	//sprobuj zobic tez innym sposobem - obiekt struktury jako argument funkcji
 	Zadanie2 prostokat;
 	prostokat.a = 10;
 	prostokat.b = 20;
@@ -28,7 +42,29 @@ int main() {
 	cout << poleprostokata(prostokat.a, prostokat.b) << endl;
 
 	//zadanie 3
+	Trojkat trojkat_1;
+	Trojkat trojkat_2;
 
+	trojkat_1 = { 2,3,4 };
+	trojkat_2 = { 5,6,8 };
+	
+	funkcja(trojkat_1, &trojkat_2);
+	*/
+	//zadanie 4*
+	Konto noweDane = {1315, 11000 };
+	Konto stareKonto = { 1315, 500 };
+
+	zaktualizuj(noweDane, &stareKonto);
+	cout << stareKonto.saldo;
+}
+void zaktualizuj(Konto noweDane, Konto* stareKonto) {
+	stareKonto->numer = noweDane.numer;
+	stareKonto->saldo = noweDane.saldo;
+}
+void funkcja(Trojkat trojkat_1, Trojkat* trojkat_2) {
+	trojkat_2->a = trojkat_1.a;
+	trojkat_2->b = trojkat_1.b;
+	trojkat_2->c = trojkat_1.c;
 }
 int poleprostokata(int x, int y) {
 	return x * y;
